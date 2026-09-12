a38ef0afe161c9092224a6723421048f6bba98ea
Rishikesh-2006 <sarmarishikesh48@gmail.com>
2026-09-12 19:47:34 +0530

    v1

--- app.py
+++ app.py
@@ -1,13 +1,39 @@
-from flask import Flask,render_template,url_for
-
+from flask import Flask,render_template,url_for,request
+import torch
+from torchvision import datasets , transforms
+import pickle
+from network import Network
 app = Flask(__name__)
+from PIL import Image
 
+  
+model = pickle.load(open('modelv2.pkl','rb'))
 
 @app.route('/')
 def home():
     css = url_for('static',filename = 'front.css')
     js = url_for('static',filename = 'draw.js')
-    return render_template('front.html',css_path = css, jspath = js)
+    saver = url_for('static',filename = 'FileSaver.js')
+    return render_template('front.html',css_path = css, drawpath = js , saver = saver)
+
+@app.route('/predict',methods = ['post'])
+def predict():
+    image = request.files["image"]
+
+    image.save("to_predict/image.png")
+
+    input = Image.open("to_predict/image.png").convert("L")
+    transform = transforms.ToTensor()
+    input = transform(input)
+    print(input.shape)
+    input = input.reshape(-1,28*28)
+
+    output = model(input)
+
+    result = torch.argmax(output,dim = 1).item()
+    print(output)
+    print(result)
+    return render_template('front.html',prediction = result)
 
 
 if __name__ == '__main__':
--- static/draw.js
+++ static/draw.js
@@ -2,15 +2,24 @@ const canvas = document.getElementById("Drawzone");
 
 let ctx = canvas.getContext("2d");
 
+const Submit = document.getElementById("pred");
 let drawval = false;
 
 canvas.width = 28;
 canvas.height = 28;
+
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+ctx.strokeStyle = "white";
+
 let x2 = 0;
 let y2 = 0;
 
 const scale = 10;
-ctx.lineWidth = 3;
+ctx.lineWidth = 5;
+ctx.lineCap = "round";
+ctx.lineJoin = "round";
 
 canvas.addEventListener("mouseup",function(){
 
@@ -46,4 +55,25 @@ canvas.addEventListener("mousedown",function(){
 
     drawval = true;
 
+});
+
+//save as png
+Submit.addEventListener("click",function(){
+    canvas.toBlob(function(blob){
+
+        const formdata = new FormData();
+        formdata.append("image",blob,"image.png");
+
+        fetch("/predict"
+            ,{
+            method : "post",
+            body : formdata
+        })
+        .then(response => response.text())
+        .then(data => {
+            document.open();
+            document.write(data);
+            document.close();
+        });
+},"image.png")  
 });
